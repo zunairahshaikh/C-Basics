cdbb4841fb7c1ac7dacceb14ebef42cc7606c034
Zunairah Shaikh <zunairahtdws@gmail.com>
2024-10-03 13:40:48 +0500

    Create Q6.c

---
+++ Lab 6/Q6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    for(int i =0; i<6; i++){
+        if(i == 0|| i==5){
+            printf("*       *\n");
+        }
+        else{
+            printf("* * * * *\n");
+        }
+    }
+
+    return 0;
+}
