2e242cad8ed239bc9cd13e632e116f9744cc583d
Zunairah Shaikh <zunairahtdws@gmail.com>
2024-10-25 09:36:43 +0500

    Create Q4.c INCOMPLETE

---
+++ Lab 8/Q4.c
@@ -0,0 +1,26 @@
+//Q. Write a C program to multiply two matrices of size 3x3 and display the result matrix. Also subtract the resultant matrix from Matrix A as well.
+
+#include <stdio.h>
+int main() {
+   int matrixA[3][3];
+   int matrixB[3][3];
+  
+    printf("Enter the elements of the 3x3 matrix A:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrixA[i][j]);
+        }
+    }
+    
+    printf("Enter the elements of the 3x3 matrix B:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrixB[i][j]);
+        }
+    }
+    
+    
+    return 0;
+}
