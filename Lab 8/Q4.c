20fab08ce2f4e8a1888a611c12e83802b04092aa
Zunairah Shaikh <zunairahtdws@gmail.com>
2024-10-30 23:07:21 +0500

    Update Q4.c

--- Lab 8/Q4.c
+++ Lab 8/Q4.c
@@ -4,8 +4,9 @@
 int main() {
    int matrixA[3][3];
    int matrixB[3][3];
-  
-    printf("Enter the elements of the 3x3 matrix A:\n");
+   int resultMatrix[3][3] = {0};
+   
+    printf("Enter the elements of the 3x3 matrix A \n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("Element [%d][%d]: ", i, j);
@@ -13,14 +14,28 @@ int main() {
         }
     }
     
-    printf("Enter the elements of the 3x3 matrix B:\n");
+    printf("Enter the elements of the 3x3 matrix B \n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("Element [%d][%d]: ", i, j);
             scanf("%d", &matrixB[i][j]);
         }
     }
-    
-    
+
+   for(int row = 0; row < 3; row++){
+    for (int col = 0; col < 3; col++) {
+        for(int i = 0; i<3; i++){
+            resultMatrix[row][col] += matrixA[row][i] * matrixB[i][col]; //i written to go down
+        }
+    }
+   }
+
+   printf("Resultant Matrix: \n");
+   for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+             printf("%d ", resultMatrix[i][j]);
+        }
+        printf("\n");
+   }
     return 0;
 }
